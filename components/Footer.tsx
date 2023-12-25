c42c5ee361e2e74984e858cb8bc87e3adf81730c
Saleh Hindi <saleh.hindi.one@gmail.com>
2023-12-25 16:15:43 -0500

    Update icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -63,9 +63,9 @@ export default function Footer() {
           </svg>
         </Link>
         <Link
-          href="https://www.linkedin.com/in/saleh-hindi-55895144/"
+          href="saleh.hindi.one@gmail.com"
           className="group"
-          aria-label="Saleh Hindi on LinkedIn"
+          aria-label="Saleh Hindi on Email"
         >
           <svg
             aria-hidden="true"
@@ -75,6 +75,18 @@ export default function Footer() {
     d="M20,4H4C2.897,4,2,4.897,2,6v12c0,1.103,0.897,2,2,2h16c1.103,0,2-0.897,2-2V6C22,4.897,21.103,4,20,4z M20,6v0.511
 l-8,6.223L4,6.512V6H20z M4,18V8.044l7.386,5.45c0.093,0.069,0.203,0.103,0.314,0.103s0.221-0.034,0.314-0.104L20,8.043V18H4z"/>
           </svg>
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/saleh-hindi-55895144/"
+          className="group"
+          aria-label="Saleh Hindi on LinkedIn"
+        >
+          <svg 
+              aria-hidden="true"
+            className="h-6 w-6 fill-slate-500 group-hover:fill-slate-700"          
+            xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+            <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401m-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4"/>
+          </svg>        
         </Link>
       </div>
     </footer>
